ded854cebe441cd5e55a084a027ebb0ea7f2e4f7
Rick <derickharon6@gmail.com>
2025-04-28 11:32:11 +0300

    added description field

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,13 +14,18 @@
 
 <table border="1">
     <thead>
-        <tr><th>Name</th><th>Price</th><th>Actions</th></tr>
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Actions</th></tr>
     </thead>
     <tbody>
     @foreach($products as $product)
         <tr>
             <td>{{ $product->name }}</td>
             <td>${{ $product->price }}</td>
+            <td>{{ $product->description }}</td>
             <td>
                 <a href="{{ route('products.edit', $product) }}">Edit</a>
                 <form action="{{ route('products.destroy', $product) }}" method="POST" style="display:inline;">
